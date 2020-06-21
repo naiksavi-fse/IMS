e369dfdfcd9e1dda4609ab3f25ea19ac545229c8
ctsdotnetfsd06@iiht.tech <ctsdotnetfsd06@iiht.tech>
2020-06-21 20:32:13 +0530

    Add project files.

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Inventory.Entities;
+using Inventory.Contracts;
+using Inventory.Entities.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Inventory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        //database
+        private IUnitOfWork _unitOfWork;
+        //logger
+        private readonly ILoggerManager _logger;
+
+       
+        public InventoryController(IUnitOfWork unitOfWork, ILoggerManager logger)
+        {
+            this._unitOfWork = unitOfWork;
+            this._logger = logger;
+        }
+
+        // GET: api/<InventoryController>
+        [HttpGet]
+        [Authorize]
+            
+        public IActionResult Get()
+        {
+            try 
+            { 
+            var currentUser = HttpContext.User;
+
+            var stocks = this._unitOfWork.Stock.FindAll();
+            _logger.LogInfo($"Returned all stocks from database.");
+            return Ok(stocks);
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetStock action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+
+        }
+        /// <summary>
+        /// Return stock details based of stock Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/<InventoryController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var result = this._unitOfWork.Stock.GetStockById(id);
+
+                if (result == null)
+                {
+                    _logger.LogError($"stock with id: {id}, hasn't been found in db.");
+                    return NotFound();
+                }
+                else
+                {
+                    _logger.LogInfo($"Returned stock with id: {id}");
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetStockById action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+
+        }
+
+        /// <summary>
+        /// add new stock to stock table
+        /// </summary>
+        /// <param name="stock"></param>
+        /// <returns></returns>
+        // POST api/<InventoryController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Stock stock)
+        {
+            try
+            {
+                if (stock == null)
+                {
+                    _logger.LogError("stock object sent from client is null.");
+                    return BadRequest("stock object is null");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogError("Invalid stock object sent from client.");
+                    return BadRequest("Invalid model object");
+                }
+
+                this._unitOfWork.Stock.Create(stock);
+                this._unitOfWork.Save();
+                return Ok("Record added successfully");
+            }
+           
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside CreateOwner action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        /// <summary>
+        /// update stock details based on Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="stock"></param>
+        /// <returns></returns>
+        // PUT api/<InventoryController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Stock stock)
+        {
+            try
+            {
+                if (stock == null)
+                {
+                    _logger.LogError("stock object sent from client is null.");
+                    return BadRequest("stock object is null");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogError("Invalid stock object sent from client.");
+                    return BadRequest("Invalid model object");
+                }
+
+                var entity = this._unitOfWork.Stock.GetStockById(id);
+                if (entity == null)
+                {
+
+                    _logger.LogError($"stock with id: {id}, hasn't been found in db.");
+                    return NotFound("No Record found");
+                }
+
+                this._unitOfWork.Stock.Update(stock);
+                this._unitOfWork.Save();
+                return Ok("Record Updated sucessfully...");
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside UpdateStock action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        /// <summary>
+        /// delete stock based on id from stock table
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // DELETE api/<InventoryController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var entity = this._unitOfWork.Stock.GetStockById(id);
+                if (entity == null)
+                {
+                    _logger.LogError($"Stock with id: {id}, hasn't been found in db.");
+                    return NotFound();
+                }
+
+                this._unitOfWork.Stock.Delete(entity);
+                this._unitOfWork.Save();
+                return Ok("Record deleted...");
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside DeleteStock action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+
+    }
+
+}
---
+++ Startup.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventory.Contracts;
+using Inventory.Entities;
+using Inventory.Repository;
+using LoggerService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using NLog;
+
+
+namespace Inventory
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            //Logger
+            LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+           
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //start
+
+              services
+             .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+             .AddJwtBearer(options =>
+             {
+                 var serverSecret = new SymmetricSecurityKey(Encoding.UTF8.
+                GetBytes(Configuration["JWT:key"]));
+                 options.TokenValidationParameters = new
+                TokenValidationParameters
+                 {
+                     IssuerSigningKey = serverSecret,
+                     ValidIssuer = Configuration["JWT:Issuer"],
+                     ValidAudience = Configuration["JWT:Audience"]
+                 };
+             });
+
+
+            //end
+            //logger
+         
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            services.AddDbContext<InventoryDBContext>(option => option.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog = InventoryDB;"));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //Logger
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, InventoryDBContext inventoryDBContext)
+        {
+            //start
+            app.UseAuthentication();
+            //end
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            //Logger
+            LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            inventoryDBContext.Database.Migrate();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
